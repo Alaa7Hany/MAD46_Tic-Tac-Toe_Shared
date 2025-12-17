e29ebbf43c83fbbd17f7218f79521bdf0711f92a
Alaa7Hany <3laahanylol@gmail.com>
2025-12-17 18:54:26 +0200

    made the library open to be exported

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module com.mycompany.tictactoeshared {
+    // Allow other projects to use my classes
+    exports com.mycompany.tictactoeshared;
+}
